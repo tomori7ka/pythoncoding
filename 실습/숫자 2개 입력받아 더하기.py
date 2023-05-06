99c82344780c99186d74ea26a6a92d098d214714
tomori7ka <132116531+tomori7ka@users.noreply.github.com>
2023-05-06 13:57:16 +0900

    Delete 실습 directory

--- 실습/숫자 2개 입력받아 더하기.py
+++
@@ -1,5 +0,0 @@
-num_1 = float(input('첫번째 숫자를 입력하세요 : '))
-num_2 = float(input('두번째 숫자를 입력하세요 : '))
-
-print('두 숫자를 더한 값은 ', num_1+num_2,' 입니다.')
-
--- 실습/숫자 입력받아 구구단.py
+++
@@ -1,6 +0,0 @@
-# 구구단 만들기
-
-number = int(input('구구단 몇 단을 출력할까요? : '))
-
-for i in range(10):
-    print(number, "x", i, "=", number*i)
