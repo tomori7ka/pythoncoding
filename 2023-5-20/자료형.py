62d040c96b02d87e67eddf560f1b4ae13bc7acc8
tomori7ka <132116531+tomori7ka@users.noreply.github.com>
2023-05-20 20:53:05 +0900

    Add files via upload

---
+++ 2023-5-20/자료형.py
@@ -0,0 +1,26 @@
+print(7 / 4)
+print(7 // 4)
+
+a = "Life is too short, You need python"
+print(a[5] + a[6])
+print(a[8:11])
+
+
+print("jinhyeung throw %d card. " % 10)
+a = 10
+b = 20
+c = a + b
+
+
+print(("블라블라 %d%%" % 100))
+
+a = "Python is the best choice"
+print(a.find("b"))
+
+print(a.find("K"))
+
+a = "Life is too short"
+print(a.index("t"))
+# print(a. index('k'))
+
+print(",".join("abcd"))
